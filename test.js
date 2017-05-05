efd7b60215f43a86490b16a1930d85b23d1d05fe
Prateek Sharma <prateek_sharma_may@yahoo.com>
2017-05-04 23:53:45 -0700

    Updates

--- test.js
+++ test.js
@@ -1,21 +1,12 @@
 /**
  * Created by Prateek on 4/22/2017.
  */
-/*var webdriver = require('selenium-webdriver');
-
-var driver = new webdriver.Builder().forBrowser('firefox').build();
-
-driver.get('http://www.google.com');
-driver.findElement(webdriver.By.name('q')).sendKeys('simple programmer');
-driver.findElement(webdriver.By.name('btnG')).click();
-driver.quit();*/
-
-
 var driver;
 var selenium = require('selenium-webdriver');
 var chai = require('chai');
 chai.use(require('chai-as-promised'));
 var expect = chai.expect;
+var until = require("selenium-webdriver/lib/until");
 
 before(function() {
     this.driver = new selenium.Builder().forBrowser('firefox').build();
@@ -23,17 +14,36 @@ before(function() {
 });
 
 after(function() {
-    return this.driver.quit();
+    // return this.driver.quit();
 });
 
 describe('Webdriver tutorial', function() {
     beforeEach(function() {
 
-        return this.driver.get('http://bites.goodeggs.com/posts/selenium-webdriver-nodejs-tutorial/');
+        return this.driver.get('https://www.groupon.com/login');
     });
-    it('has the title of the post in the window\'s title', function() {
-        return expect(this.driver.getTitle()).to.eventually.contain('Getting started with Selenium Webdriver for node.js');
+    it('has the Groupon Title in the window\'s title', function(){
+        var uname = "groupontest102@gmail.com";
+        var pswrd = "Passwd!1";
+        console.log(typeof uname);
+        this.driver.findElement(selenium.By.id("email-input")).sendKeys('groupontest102@gmail.com');
+        this.driver.findElement(selenium.By.id("password-input")).sendKeys('Passwd!1');
+        /*selenium.executeScript("document.getElementById('email-input').setAttribute('value', uname)");
+        selenium.executeScript("document.getElementById('password-input').setAttribute('value', pswrd)");*/
+
+
+        // this.driver.findElement({ xpath : "/html/body/div[1]/div[2]/section[2]/div/div[2]/div[1]/figure[2]/a/div/div[2]/div[6]/div"}).click();
+        return expect(this.driver.getTitle()).to.eventually.contain('Groupon: Deals and Coupons for Restaurants, Fitness, Travel, Shopping, Beauty, and more.');
     });
+
+    /*it('has the Groupon Title in the window\'s title', function() {
+
+        this.driver.findElement({id:"nothx"}).click();
+        this.driver.findElement({ linkText : "Cart"}).click();
+        // this.driver.findElement({ xpath : "/html/body/div[1]/div[2]/section[2]/div/div[2]/div[1]/figure[2]/a/div/div[2]/div[6]/div"}).click();
+        return expect(this.driver.getTitle()).to.eventually.contain('Groupon: Deals and Coupons for Restaurants, Fitness, Travel, Shopping, Beauty, and more.');
+    });*/
+    /*
     it('has publication date', function() {
         var text;
         text = this.driver.findElement({
@@ -46,5 +56,5 @@ describe('Webdriver tutorial', function() {
             linkText: 'Home'
         }).click();
         return expect(this.driver.getCurrentUrl()).to.eventually.equal('https://team.goodeggs.com/');
-    });
+    });*/
 });
